eeb0947c90e6c504ee03b3f46cf5fdafeae8f962
johnwilliam <jsidhom1@jhmi.edu>
2019-08-01 16:46:18 -0400

    initial_commit

--- Run_T.py
+++ Run_T.py
@@ -9,7 +9,7 @@ cluster_file = 'GBM Single Cell Data Share/072519/idents/tIdents.RDS'
 id_file = 'JW_data/t_id.csv'
 gene_file = 'JW_data/t_genes.csv'
 
-JW_obj.Load_Data(data_file,id_file,gene_file,mnn_file,Load_Prev_Data=False)
+JW_obj.Load_Data(data_file,id_file,gene_file,mnn_file,Load_Prev_Data=True)
 JW_obj.Run_UMAP(Load_Prev_Data=True)
 JW_obj.Load_Clustering(cluster_file,Load_Prev_Data=True)
 # By_Gene, By_Sample, By_Cluster
